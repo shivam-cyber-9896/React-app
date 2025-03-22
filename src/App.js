61f04268531e8ddd247412a6650b5c4ea53be126
shivam khatri <shivamkhatri98960@gmail.com>
2025-03-22 13:11:21 +0530

    jghjhgfdg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [first,setFirst]=useState(20)
+  const [second,setSecond]=useState(40)
+  const [total,setTotal]=useState(0)
+  const [flag,setFlag]=useState(true)
+  function shownew()
+  {
+     setTotal(first+second)
+  }
+  function toggle()
+  {
+    setFlag(!flag)
+  }
+  console.log("component render")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="backred">
+   <h1>Total sum is :{total}</h1>
+   {
+    flag?"flag is true":"flag is false"
+   }
+   <h1>hello react</h1>
+  
+   
+   <button onClick={()=>shownew()}>show</button>
+   <button onClick={toggle}>Change status</button>
     </div>
   );
 }
 
-export default App;
+export default App;
+
